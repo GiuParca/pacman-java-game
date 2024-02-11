6c0f84ef5ca233557ced27edcfd63e666027fa58
Giulia Parca <124640350+GiuParca@users.noreply.github.com>
2024-02-11 16:33:57 +0000

    Creating pacman repo

---
+++ Field.java
@@ -0,0 +1,116 @@
+public class Field {
+
+    Picture mapa = new Picture(10, 10,"lib/FundoPacman.jpeg");
+    mapa.draw();
+    Rectangle rectangle = new Rectangle(86,88,80,52);
+    rectangle.setColor(Color.GREEN);
+    rectangle.fill();
+
+    Rectangle rectangle1 = new Rectangle(225, 88, 115, 52);
+    rectangle1.setColor(Color.BLUE);
+    rectangle1.fill();
+
+    Rectangle rectangle2 = new Rectangle(485, 88, 115, 52);
+    rectangle2.setColor(Color.BLUE);
+    rectangle2.fill();
+
+    Rectangle rectangle3 = new Rectangle(655, 88, 80, 52);
+    rectangle3.setColor(Color.BLUE);
+    rectangle3.fill();
+
+    Rectangle rectangle4 = new Rectangle(85, 200, 85, 30);
+    rectangle4.setColor(Color.BLUE);
+    rectangle4.fill();
+
+    Rectangle rectangle5 = new Rectangle(230, 200, 25, 200);
+    rectangle5.setColor(Color.BLUE);
+    rectangle5.fill();
+
+    Rectangle rectangle6 = new Rectangle(254, 283, 85, 30);
+    rectangle6.setColor(Color.BLUE);
+    rectangle6.fill();
+
+    Rectangle rectangle7 = new Rectangle(310, 200, 200, 25);
+    rectangle7.setColor(Color.BLUE);
+    rectangle7.fill();
+
+    Rectangle rectangle8 = new Rectangle(400, 225, 30, 85);
+    rectangle8.setColor(Color.ORANGE);
+    rectangle8.fill();
+
+    Rectangle rectangle9 = new Rectangle(565, 200, 30, 200);
+    rectangle9.setColor(Color.GRAY);
+    rectangle9.fill();
+
+    Rectangle rectangleesquisito = new Rectangle(480, 285, 85, 30);
+    rectangleesquisito.setColor(Color.BLUE);
+    rectangleesquisito.fill();
+
+    Rectangle rectangle10 = new Rectangle(655, 200, 85, 30);
+    rectangle10.setColor(Color.MAGENTA);
+    rectangle10.fill();
+
+    Rectangle rectangle11 = new Rectangle(230, 450, 30, 110);
+    rectangle11.setColor(Color.BLUE);
+    rectangle11.fill();
+
+    Rectangle rectangle12 = new Rectangle(570, 450, 30, 110);
+    rectangle12.setColor(Color.BLUE);
+    rectangle12.fill();
+
+    Rectangle rectangle13 = new Rectangle(310, 540, 200, 25);
+    rectangle13.setColor(Color.BLUE);
+    rectangle13.fill();
+
+    Rectangle rectangle14 = new Rectangle(395, 565, 30, 85);
+    rectangle14.setColor(Color.CYAN);
+    rectangle14.fill();
+
+    Rectangle rectangle15 = new Rectangle(80, 625, 60, 25);
+    rectangle15.setColor(Color.BLUE);
+    rectangle15.fill();
+
+    Rectangle rectangle16 = new Rectangle(140, 625, 25, 110);
+    rectangle16.setColor(Color.BLUE);
+    rectangle16.fill();
+
+    Rectangle rectangle17 = new Rectangle(225, 625,  115, 25);
+    rectangle17.setColor(Color.BLUE);
+    rectangle17.fill();
+
+    Rectangle rectangle18 = new Rectangle(490, 625, 115, 25);
+    rectangle18.setColor(Color.BLUE);
+    rectangle18.fill();
+
+    Rectangle rectangle19 = new Rectangle(655, 625, 30, 110);
+    rectangle19.setColor(Color.BLUE);
+    rectangle19.fill();
+
+    Rectangle rectangle20 = new Rectangle(680, 625, 60, 25);
+    rectangle20.setColor(Color.BLUE);
+    rectangle20.fill();
+
+    Rectangle rectangle21 = new Rectangle(85, 795 , 255, 30);
+    rectangle21.setColor(Color.BLUE);
+    rectangle21.fill();
+
+    Rectangle rectangle22 = new Rectangle(230, 710, 25, 85);
+    rectangle22.setColor(Color.BLUE);
+    rectangle22.fill();
+
+    Rectangle rectangle23 = new Rectangle(310, 710, 200, 30);
+    rectangle23.setColor(Color.BLUE);
+    rectangle23.fill();
+
+    Rectangle rectangle24 = new Rectangle(400, 740,  25, 80);
+    rectangle24.setColor(Color.BLUE);
+    rectangle24.fill();
+
+    Rectangle rectangle25 = new Rectangle(570, 710,  25, 80);
+    rectangle25.setColor(Color.BLUE);
+    rectangle25.fill();
+
+    Rectangle rectangle26 = new Rectangle(485, 795,  250, 30);
+    rectangle26.setColor(Color.BLUE);
+    rectangle26.fill();
+}
